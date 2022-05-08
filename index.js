f6cb5c6b508c41eaebe18483cad3052020b67724
Jesús Valdez <jsvaldezv@yahoo.com>
2022-05-08 13:50:19 -0500

    Drum line with js done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Init");
+const drumsInstruments = ["Kick", "Snare", "Hihat", "Tom", "Clap"]
+
+function getDrumLine(inInstrument)
+{
+	var drumLine = document.createElement('div');
+	drumLine.className = 'drumLine';
+
+	drumLine.innerHTML = "<div class='drumInstrument'>" + inInstrument + "</div>";
+	
+	for(let i = 0; i < 16; i++) 
+	{
+		drumLine.innerHTML += "<button type='button' class='drumPad'></button>";
+	}
+
+	return drumLine;
+}
+
+function createDrumMachine()
+{
+	const drumsContainer = document.querySelector('.drumContainer');
+
+	for(var i = 0; i < drumsInstruments.length; i++)
+	{
+		console.log(drumsInstruments[i]);
+		drumsContainer.appendChild(getDrumLine(drumsInstruments[i]));
+	}	
+}
+
+window.onload = createDrumMachine;
