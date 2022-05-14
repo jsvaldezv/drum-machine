99d597c376233fc7c70b90826e31f35c186d7c77
Jesús Valdez <jsvaldezv@yahoo.com>
2022-05-13 22:07:01 -0500

    Num instruments working

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 import Timer from "./timer.js"
 
+const okNumInstruments = document.querySelector(".okNumInstruments");
+
 // GLOBAL VARIABLES
-let num = window.prompt("Número de instrumentos: ");
 let contWhile = 0;
 let drumPads;
 let drumsInstruments = [];
 let padStates = [];
-
 let bpm = 120;
 let beatsPerMeasure = 4;
 
 // ADD INSTRUMENTS FOR DRUM MACHINE
-while (contWhile < num)
+/*while (contWhile < num)
 {
 	drumsInstruments.push(window.prompt("Instrumento: "));
 
@@ -19,7 +19,7 @@ while (contWhile < num)
 		padStates.push(false);
 	
 	contWhile++;
-}
+}*/
 
 // ADD LISTENERS
 function addListeners()
@@ -77,8 +77,37 @@ function playClick()
 	console.log("Click");
 }
 
-// EXECUTE FUNCTION ON LOADING
-window.onload = createDrumMachine;
+function getUserInstrumentChoice()
+{
+	let drumInstrumentChoice = document.createElement('div');
+	drumInstrumentChoice.className = 'drumInstrumentChoice';
+	drumInstrumentChoice.innerHTML = 
+		`<form>
+			<select id="country" name="country">
+			<option value="kick">Kick</option>
+			<option value="snare">Snare</option>
+			<option value="hihat">Hihat</option>
+			<option value="hihat">Tom</option>
+			<option value="hihat">Crash</option>
+			</select>
+  		</form>`;
+
+	return drumInstrumentChoice;
+}
+
+function createInstrumentChoices()
+{
+	const numInstruments = document.getElementById('numInstruments');
+	const currentWindows = document.querySelector(".drumUserInfo");
+	const drumsUserChoice = document.querySelector(".drumsUserChoice");
+
+	currentWindows.style.display = "none";
+
+	for(let i = 0; i < numInstruments.value; i++)
+		drumsUserChoice.appendChild(getUserInstrumentChoice());
+}
+
+okNumInstruments.addEventListener('click', createInstrumentChoices);
 
 const metronome = new Timer(playClick, 60000/bpm, {inmediate: true})
 //metronome.start();
