126319d6633a212a64b2813d2ffc873eba9a56ce
Jesús Valdez <jsvaldezv@yahoo.com>
2022-05-13 23:27:51 -0500

    Icon play pause change done

--- js/dsp.js
+++ js/dsp.js
@@ -1,13 +1,56 @@
+//****************************************************************************************************/
+//********************************************* DSP DRUM MACHINE *************************************/
+//****************************************************************************************************/
+
+//************************************************* IMPORTS ******************************************/
 import Timer from "./timer.js"
 
-// METRONOME
+//**************************************** *****INITIAL SELECTORS ************************************/
+const playSelector = document.querySelector(".play");
+const pauseSelector = document.querySelector(".pause");
+
+//**************************************** GLOBAL VARIABLES ******************************************/
 let bpm = 120;
 let beatsPerMeasure = 4;
 
-function playClick()
+//**************************************** SHOW/HIDE ICONS ******************************************/
+function showHide(inIcons) 
 {
-	console.log("Click");
+	if(inIcons == "play")
+	{
+		playSelector.classList.remove("hide");
+		playSelector.classList.add("show");
+
+		pauseSelector.classList.remove("show");
+		pauseSelector.classList.add("hide");
+	}
+
+	else
+	{
+		playSelector.classList.remove("show");
+		playSelector.classList.add("hide");
+
+		pauseSelector.classList.remove("hide");
+		pauseSelector.classList.add("show");
+	}
 }
 
-const metronome = new Timer(playClick, 60000/bpm, {inmediate: true})
-//metronome.start();
+//********************************************* PLAY *************************************************/
+function play()
+{
+	console.log("Click");
+	showHide("stop");
+	//metronome.start();
+}
+
+function stop()
+{
+	showHide("play");
+}
+
+//******************************************* CREATE TIMER OBJECT ************************************/
+const metronome = new Timer(play, 60000/bpm, {inmediate: true})
+
+//******************************************** PLAY LISTENER  ***************************************/
+playSelector.addEventListener('click', play);
+pauseSelector.addEventListener('click', stop);
